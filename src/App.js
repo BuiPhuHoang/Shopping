7a3a1e83e7567d176c325f3f537384addcc9df6c
hoang.bui <buiphuhoang@gmail.com>
2019-05-25 22:06:45 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { Text, View, TextInput, Image, StatusBar } from 'react-native'
 import HomeScreen   from './Component/HomeScreen/HomeScreen'
-import Stack from './Navigation/Navigation'
+import Drawer from './Navigation/Navigation'
 export default class App extends Component {
     render() {
         return (
-            <View style={{ backgroundColor: 'green', width: '100%', height: 100 }}>
-               <Stack />
+            <View style={{ backgroundColor: 'green', flex:1 }}>
+               <Drawer />
             </View>
         );
     }
--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,23 +1,44 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import HomeScreen from '../Component/HomeScreen/HomeScreen'
 import Cart from '../Component/CartAndCheck/Cart'
 import CheckAlert from '../Component/CartAndCheck/Check'
 import Search from '../Component/SearchAndContact/Search'
 import Contact from '../Component/SearchAndContact/Contact'
-import {createAppContainer,createBottomTabNavigator,createDrawerNavigator,createStackNavigator} from 'react-navigation'
- 
+import { createAppContainer, createBottomTabNavigator, createDrawerNavigator, createStackNavigator } from 'react-navigation'
+
 export const Stack = createBottomTabNavigator({
-    ManhinhHome:{
-        screen: HomeScreen
+    ManhinhHome: {
+        screen: HomeScreen,
+        navigationOptions:{
+            title:'HOME'
+        } 
     },
-    ManhinhCart:{
+    ManhinhCart: {
         screen: Cart
     },
-    ManhinhSearch:{
+    ManhinhSearch: {
         screen: Search
     },
-    ManhinhContact:{
+    ManhinhContact: {
         screen: Contact
     }
-}) 
-export default createAppContainer(Stack);
+},
+    {
+        tabBarOptions: {
+            showIcon: false,
+            tabStyle: {
+                justifyContent:'center',
+                alignItems:'center'
+            }
+        }
+    })
+export const Drawer= createDrawerNavigator({
+    Home:{
+        screen: HomeScreen
+    },
+    Contact:{
+        screen: Contact
+    }
+        
+})
+export default createAppContainer(Drawer);
