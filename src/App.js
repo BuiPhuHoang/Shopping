e9f75d9af921c0ca8f9359566b9c7af827fdf602
thinhbui <buithinh2810@gmail.com>
2019-05-25 23:47:28 +0700

    tao content component drawer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react'
-import { Text, View, TextInput, Image, StatusBar } from 'react-native'
-import HomeScreen   from './Component/HomeScreen/HomeScreen'
-import Stack from './Navigation/Navigation'
+import React, { Component } from 'react';
+import { Text, View, TextInput, Image, StatusBar } from 'react-native';
+import HomeScreen from './Component/HomeScreen/HomeScreen';
+import Bottom from './Navigation/Navigation';
 export default class App extends Component {
-    render() {
-        return (
-            <View style={{ backgroundColor: 'green', width: '100%', height: 100 }}>
-               <Stack />
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View
+        style={{
+          backgroundColor: 'green',
+          flex: 1
+        }}
+      >
+        <Bottom />
+      </View>
+    );
+  }
 }
--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,23 +1,108 @@
-import React,{Component} from 'react'
-import HomeScreen from '../Component/HomeScreen/HomeScreen'
-import Cart from '../Component/CartAndCheck/Cart'
-import CheckAlert from '../Component/CartAndCheck/Check'
-import Search from '../Component/SearchAndContact/Search'
-import Contact from '../Component/SearchAndContact/Contact'
-import {createAppContainer,createBottomTabNavigator,createDrawerNavigator,createStackNavigator} from 'react-navigation'
- 
-export const Stack = createBottomTabNavigator({
-    ManhinhHome:{
-        screen: HomeScreen
+import React, { Component } from 'react';
+import HomeScreen from '../Component/HomeScreen/HomeScreen';
+import Cart from '../Component/CartAndCheck/Cart';
+import CheckAlert from '../Component/CartAndCheck/Check';
+import Search from '../Component/SearchAndContact/Search';
+import { Text, View, Image } from 'react-native';
+import Contact from '../Component/SearchAndContact/Contact';
+import {
+  createAppContainer,
+  createBottomTabNavigator,
+  createDrawerNavigator,
+  createStackNavigator
+} from 'react-navigation';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+export const Bottom = createBottomTabNavigator({
+  ManhinhHome: {
+    screen: HomeScreen
+  },
+  ManhinhCart: {
+    screen: Cart
+  },
+  ManhinhSearch: {
+    screen: Search
+  },
+  ManhinhContact: {
+    screen: Contact
+  }
+});
+export const Drawer = createDrawerNavigator(
+  {
+    Stack: {
+      screen: Bottom,
+      navigationOptions: {
+        title: 'bao stack'
+      }
     },
-    ManhinhCart:{
-        screen: Cart
-    },
-    ManhinhSearch:{
-        screen: Search
-    },
-    ManhinhContact:{
-        screen: Contact
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        title: 'Homehehe'
+      }
     }
-}) 
-export default createAppContainer(Stack);
+  },
+  {
+    contentComponent: props => {
+      return (
+        <View style={{ flex: 1 }}>
+          <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+          >
+            <Image
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                borderColor: 'gray',
+                borderWidth: 1.5
+              }}
+              source={{
+                uri:
+                  'https://sites.google.com/site/hinhanhdep24h/_/rsrc/1436687439788/home/hinh%20anh%20thien%20nhien%20dep%202015%20%281%29.jpeg'
+              }}
+            />
+            <Text>{'Hoang Bui'}</Text>
+          </View>
+          <View style={{ flex: 2, alignItems: 'center' }}>
+            <TouchableOpacity
+              onPress={() => {
+                props.navigation.navigate('ManhinhContact');
+              }}
+            >
+              <Text>{'Shop'}</Text>
+            </TouchableOpacity>
+            <View
+              style={{
+                backgroundColor: 'gray',
+                width: '100%',
+                height: 1,
+                marginTop: 10
+              }}
+            />
+            <Text>{'Change Information'}</Text>
+            <View
+              style={{
+                backgroundColor: 'gray',
+                width: '100%',
+                height: 1,
+                marginTop: 10
+              }}
+            />
+            <Text>{'Order History'}</Text>
+            <View
+              style={{
+                backgroundColor: 'gray',
+                width: '100%',
+                height: 1,
+                marginTop: 10
+              }}
+            />
+            <Text>{'Sign In'}</Text>
+          </View>
+        </View>
+      );
+    }
+  }
+);
+export default createAppContainer(Drawer);
